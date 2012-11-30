f12e2d287e0a96c979a60d663d169e791df7b11b
Heinz N. Gies <heinz@licenser.net>
2012-11-30 03:08:25 +0100

    Added rebar, and made compile.

--- src/fifo.erl
+++ src/fifo.erl
@@ -1,1 +1,20 @@
 -module(fifo).
+
+
+-export_type([config_list/0,
+	      config/0,
+	      package/0,
+	      dataset/0,
+	      uuid/0]).
+
+-type config_list() :: [{Key::binary(), Value::term()}].
+
+-type config() :: config_list().
+
+-type package() :: config_list().
+
+-type dataset() :: config_list().
+
+-type uuid() :: binary().
+
+
